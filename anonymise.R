d2b509f93d586c1e593001dfc0ed921c14f39258
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2017-08-28 20:52:13 +1000

    function to anonymise data in data.table

--- anonymise.R
+++ anonymise.R
@@ -1,15 +1,22 @@
-# Funciton to anonymise data in a data.frame
-# in code body use cols_to_mask as a vector witht he column names to anonymise
+## Function to anonymise data in a data.frame
+# ==========================================
+# In code body use cols_to_anon as a vector with the column names to anonymise
 # e.g. cols_to_mask <- c("name","address","post-code)
-# then call the funciton to anonymise the data.frame
-# data[,cols_to_mask := lapply(.SD, anonymise), .SDcols_to_mask, with=FALSE]
+# then call the function to anonymise the data.table e.g.
+# anonymised <- anonymise(data, cols_to_anon)
+# OR
+# dt[,cols_to_anon] <- anonymise(dt,cols_to_anon)
+# Needs data.table - may return error if passed data.frame
 
 # algo=c("md5", "sha1", "crc32", "sha256", "sha512","xxhash32", "xxhash64", "murmur32")
 
-# code by Jan Górecki, posted November 6, 2014
-# https://www.r-bloggers.com/data-anonymization-in-r/
+# code by richierocks, posted August 23, 2011
+#https://www.r-bloggers.com/anonymising-data/
+# ==========================================
 
-anonymise <- function(x, algo="crc32") {
-  unq_hases <- vapply(unique(x), function(object) digest(object, algo=algo), FUN.VALUE="", USE.NAMES=TRUE)
-  unname(unq_hases[x])
+function(data, cols_to_anon, algo = "crc32")
+{
+  if(!require(digest)) stop("digest package is required")
+  to_anon <- subset(data, select = cols_to_anon)
+  unname(apply(to_anon, 1, digest, algo = algo))
 }
